01dea218aa2106885e557beecc3dfd67caa34b20
brandaoplaster <brandaoplaster@gmail.com>
2024-08-16 23:06:08 -0300

    feat: Adding command for actions

--- commands/controller.go
+++ commands/controller.go
@@ -3,50 +3,59 @@ package commands
 import (
 	"fmt"
 	"os"
+	"strings"
 	"text/template"
+	"unicode"
 
 	"github.com/spf13/cobra"
 )
 
+// Define o comando 'GenerateCmd' como uma variável exportada
 var GenerateCmd = &cobra.Command{
 	Use:   "generation",
 	Short: "Generate files for the project",
 }
 
+// Define o subcomando 'controllerCmd'
 var controllerCmd = &cobra.Command{
-	Use:   "controller [name]",
-	Short: "Generate a new controller",
-	Args:  cobra.ExactArgs(1),
+	Use:   "controller [name] [actions...]",
+	Short: "Generate a new controller with specified actions",
+	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		controllerName := args[0]
-		generateController(controllerName)
+		actions := args[1:]
+		generateController(controllerName, actions)
 	},
 }
 
 func init() {
+	// Adiciona o subcomando 'controllerCmd' ao comando 'GenerateCmd'
 	GenerateCmd.AddCommand(controllerCmd)
 }
 
-func generateController(name string) {
+// Função para gerar um novo arquivo de controller
+func generateController(name string, actions []string) {
 	tmpl := `package controllers
 
 import "net/http"
 
 type {{.Name}}Controller struct{}
 
-func (c *{{.Name}}Controller) Index(w http.ResponseWriter, r *http.Request) {
-	// Handle index
+{{range .Actions}}
+func (c *{{$.Name}}Controller) {{.}}(w http.ResponseWriter, r *http.Request) {
+	// Handle {{.}}
 }
-
-func (c *{{.Name}}Controller) Show(w http.ResponseWriter, r *http.Request) {
-	// Handle show
-}
+{{end}}
 `
 	data := struct {
-		Name string
+		Name    string
+		Actions []string
 	}{
-		Name: name,
+		Name:    name,
+		Actions: actions,
 	}
+
+	data.Actions = CapitalizeSlice(data.Actions)
 
 	t := template.Must(template.New("controller").Parse(tmpl))
 
@@ -64,5 +73,31 @@ func (c *{{.Name}}Controller) Show(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Printf("Controller '%s' created successfully.\n", name)
+	fmt.Printf("Controller '%s' with actions %s created successfully.\n", name, strings.Join(actions, ", "))
+}
+
+func Capitalize(s string) string {
+	var result strings.Builder
+	capitalize := true
+
+	for _, r := range s {
+		if unicode.IsSpace(r) {
+			capitalize = true
+			result.WriteRune(r)
+		} else if capitalize && unicode.IsLetter(r) {
+			result.WriteRune(unicode.ToUpper(r))
+			capitalize = false
+		} else {
+			result.WriteRune(r)
+		}
+	}
+
+	return result.String()
+}
+
+func CapitalizeSlice(slice []string) []string {
+	for i, str := range slice {
+		slice[i] = Capitalize(str)
+	}
+	return slice
 }
